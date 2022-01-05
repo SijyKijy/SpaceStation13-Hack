e82abff0e6a5aad117260395f92bb24f22963d58
SijyKijy <SijyKijy@users.noreply.github.com>
2022-01-06 00:44:29 +0300

    Fix fullbright

--- src/SpaceStation13-Cheat/Addresses.h
+++ src/SpaceStation13-Cheat/Addresses.h
@@ -10,7 +10,7 @@ namespace Addresses {
 	char Wallhack_on[] = "\xBA\x3C\x00\x00\x00\x90";
 
 	// Fullbright
-	const char* Fullbright_pattern = "0F B6 83 29 02 00 00";
-	char Fullbright_off[] = "\x0F\xB6\x83\x29\x02\x00\x00";
-	char Fullbright_on[] = "\xB8\x55\x02\x00\x00\x66\x90";
+	const char* Fullbright_pattern = "0F B6 88 2A 02 00 00 0F BE 46 38";
+	char Fullbright_off[] = "\x0F\xB6\x88\x2A\x02\x00\x00\x0F\xBE\x46\x38";
+	char Fullbright_on[] = "\xB9\x01\x00\x00\x00\x66\x90\x0F\xBE\x46\x38";
 }
