fd7a32b9375ec303dc5eb72290e5712aabe35524
SijyKijy <SijyKijy@users.noreply.github.com>
2022-01-06 00:19:10 +0300

    Add new helper method

--- src/SpaceStation13-Cheat/Helper.cpp
+++ src/SpaceStation13-Cheat/Helper.cpp
@@ -11,3 +11,63 @@ DWORD GetPointerAddress(DWORD address, std::vector<DWORD> offsets)
 
     return newAddress;
 }
+
+uint8_t* AOBScan(void* module, const char* signature)
+{
+	static auto pattern_to_byte = [](const char* pattern) {
+		auto bytes = std::vector<int>{};
+		auto start = const_cast<char*>(pattern);
+		auto end = const_cast<char*>(pattern) + strlen(pattern);
+
+		for (auto current = start; current < end; ++current) {
+			if (*current == '?') {
+				++current;
+				if (*current == '?')
+					++current;
+				bytes.push_back(-1);
+			}
+			else {
+				bytes.push_back(strtoul(current, &current, 16));
+			}
+		}
+		return bytes;
+	};
+
+	auto dosHeader = (PIMAGE_DOS_HEADER)module;
+	auto ntHeaders = (PIMAGE_NT_HEADERS)((std::uint8_t*)module + dosHeader->e_lfanew);
+
+	auto sizeOfImage = ntHeaders->OptionalHeader.SizeOfImage;
+	auto patternBytes = pattern_to_byte(signature);
+	auto scanBytes = reinterpret_cast<std::uint8_t*>(module);
+
+	auto s = patternBytes.size();
+	auto d = patternBytes.data();
+
+	for (auto i = 0ul; i < sizeOfImage - s; ++i) {
+		bool found = true;
+		for (auto j = 0ul; j < s; ++j) {
+			if (scanBytes[i + j] != d[j] && d[j] != -1) {
+				found = false;
+				break;
+			}
+		}
+		if (found) {
+			return &scanBytes[i];
+		}
+	}
+	return nullptr;
+}
+
+void Patch(char* dst, char* src, int size) {
+	DWORD oldprotect;
+	VirtualProtect(dst, size, PAGE_READWRITE, &oldprotect);
+	memcpy(dst, src, size);
+	VirtualProtect(dst, size, oldprotect, &oldprotect);
+}
+
+void Nop(char* dst, unsigned int size) {
+	DWORD oldprotect;
+	VirtualProtect(dst, size, PAGE_READWRITE, &oldprotect);
+	memset(dst, 0x90, size);
+	VirtualProtect(dst, size, oldprotect, &oldprotect);
+}
--- src/SpaceStation13-Cheat/Helper.h
+++ src/SpaceStation13-Cheat/Helper.h
@@ -3,3 +3,6 @@
 #include <vector>
 
 DWORD GetPointerAddress(DWORD ptr, std::vector<DWORD> offsets);
+uint8_t* AOBScan(void* module, const char* signature);
+void Patch(char* dst, char* src, int size);
+void Nop(char* dst, unsigned int size);
